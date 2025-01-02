1bd66e917b3bf7cea38fd14e4528b106b9832638
Martin Mwangi <88835433+wotengs@users.noreply.github.com>
2025-01-02 21:02:13 +0300

    Update api_constants.dart

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,2 +1,2 @@
 const String BASE_URL = "https://api.openai.com/v1";
-const String API_KEY = "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";
+const String API_KEY = "";
